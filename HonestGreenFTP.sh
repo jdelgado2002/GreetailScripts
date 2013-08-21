9edf72e4d9cb6db542bf1cd0eeac69411d1c72cc
Jose Delgado <jdelgado@greetail.co>
2013-08-21 12:45:17 -0500

    working import

--- HonestGreenFTP.sh
+++ HonestGreenFTP.sh
@@ -1,5 +1,14 @@
-HOST='ftp.mozilla.org'
-ftp <<END_SCRIPT
-open $HOST
-get /pub/mozilla.org/README
-END_SCRIPT
+#!/bin/bash
+HOST='ginkgo.unfi.com'
+USER='daily_file_access'
+PASSWD='dafiftp01!'
+ftp -n $HOST << END 
+quote USER $USER
+quote PASS $PASSWD
+bin
+prompt off
+cd /DailyFiles	 # this folder contains files...
+lcd /home/mayiyo/Documents/DailyImport	# this location is local directory 
+mget hgproductfeed.csv	
+bye
+END
